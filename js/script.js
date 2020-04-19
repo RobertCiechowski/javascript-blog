b3c4b938aeab1d184f7b01fe9e90640b5a03baf1
Robert Ciechowski <croberttm@gmail.com>
2020-04-19 02:24:35 +0200

    Add GENERATE TITLE LIST code to script.js file and delete list elements from html file

--- js/script.js
+++ js/script.js
@@ -42,8 +42,61 @@ function titleClickHandler(event){
     console.log('added class active to: ' + articleSelector);    
 }
 
-const links = document.querySelectorAll('.titles a');
+// ***              ***
+// GENERATE TITLE LIST
+// ***              ***
 
-for(let link of links){
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+    console.log('GENERATE TITLE LIST');
+
+    /* [DONE] remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log('Contents od titleList removed');
+
+    /* [DONE] for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(let article of articles){    
+
+        /* [DONE] get the article id */
+
+        const articleId = article.getAttribute("id");
+        console.log('id attribute get from the article: ' + articleId);
+
+        /* [DONE] find the title element */
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+ 
+        /* [DONE] get the title from the title element */
+
+        console.log(articleTitle);
+
+        /* [DONE] create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+
+        /* [IN PROGRESS] insert link into titleList */
+
+        html = html + linkHTML;
+        //console.log(html);
+    
+    }
+    titleList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+}
+
+generateTitleLinks();
